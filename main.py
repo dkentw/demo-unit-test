def95ccb8d8c1fb33d9b6582c6a42c2ba153f60b
dken <dken.tw@gmail.com>
2015-09-29 12:02:00 +0800

    udpate demo-4

--- main.py
+++ main.py
@@ -15,16 +15,24 @@ class Hash:
     def get_hash(self, password):
         return 'abcd' if password == '123' else None
 
+class MobileManager:
+    def send_text(self, user, msg):
+        print '{0} sent a message: {1}'.format(user, msg)
+    
     
 class ValidateAccount:
-    def __init__(self, account_db, passwd_hash):
+    def __init__(self, account_db, passwd_hash, mobile_manager):
         self.account_db = account_db
         self.passwd_hash = passwd_hash
+        self.mobile_manager = mobile_manager
         
     def check_account(self, id, password):
         password_by_id = self.account_db.get_password(id)
         password_by_hash = self.passwd_hash.get_hash(password)
         if password_by_id == password_by_hash:
+            self.mobile_manager.send_text(id, 'Account Valid')  # send sms
             return True
         else:
+            self.mobile_manager.send_text(id, 'Account Not Valid')  # send sms
             return False
+            
--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import unittest
+from mock import MagicMock
 
 from main import ValidateAccount
 from main import AccountSqlite
 from main import Hash
+from main import MobileManager
 
 # Stubs
 class StubAccountDB:
@@ -19,20 +21,37 @@ class StubHash2:
     def get_hash(self, password):
         return 'abcde'
     
-    
 # Tests    
 class TestValidator(unittest.TestCase):
+    def setUp(self):
+        mobile = MobileManager()
+        self.mockMobile = MagicMock(spec=mobile)  # Mock object
+        
     def test_validate_valid_passwd(self):
         account_db = StubAccountDB()
         passwd_hash = StubHash()
-        validator = ValidateAccount(account_db, passwd_hash)
+        validator = ValidateAccount(account_db, passwd_hash, self.mockMobile)
         self.assertTrue(validator.check_account('kent', '123'))
         
     def test_validate_invalid_passwd(self):
         account_db = StubAccountDB()
         passwd_hash = StubHash2()
-        validator = ValidateAccount(account_db, passwd_hash)
+        validator = ValidateAccount(account_db, passwd_hash, self.mockMobile)
         self.assertFalse(validator.check_account('kent', '1234'))
+        
+    def test_send_sms_when_account_valid(self):
+        account_db = StubAccountDB()
+        passwd_hash = StubHash()
+        validator = ValidateAccount(account_db, passwd_hash, self.mockMobile)
+        validator.check_account('kent', '1234')
+        self.mockMobile.send_text.assert_called_with('kent', 'Account Valid')  # Use mock object to assert.
+        
+    def test_send_sms_when_account_valid(self):
+        account_db = StubAccountDB()
+        passwd_hash = StubHash2()
+        validator = ValidateAccount(account_db, passwd_hash, self.mockMobile)
+        validator.check_account('kent', '1234')
+        self.mockMobile.send_text.assert_called_with('kent', 'Account Not Valid')  # Use mock object to assert.
         
 suite = unittest.TestLoader().loadTestsFromTestCase(TestValidator)
 unittest.TextTestRunner(verbosity=2).run(suite)
